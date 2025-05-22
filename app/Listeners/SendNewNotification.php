da66eed36b93a9361bba82525c7959f7e945def2
SHAVKAT BEGMATOV <62874217+shavkatbegmatov@users.noreply.github.com>
2025-05-22 15:51:38 +0500

    add note to telegram chat

--- app/Listeners/SendNewNotification.php
+++ app/Listeners/SendNewNotification.php
@@ -7,27 +7,23 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Http;
 
-class SendNewNotification
+class SendNewNotification implements ShouldQueue
 {
     use InteractsWithQueue;
-
-    public function __construct()
-    {
-        //
-    }
 
     public function handle(NewLeadCreated $event): void
     {
         $lead = $event->lead;
-        $text = " *New Lead*: {$lead->name}\n}".
+        $text = " *New Lead*: {$lead->name}\n".
                 " *Phone Number*: {$lead->phone}\n".
                 " *Car Model*: {$lead->car_model}\n".
+                " *Text*: {$lead->note}\n".
                 " *Created At*: {$lead->created_at->format('Y-m-d H:i:s')}";
 
         Http::post("https://api.telegram.org/bot".config('services.telegram.bot_token').'/sendMessage',[
             'chat_id' => config('services.telegram.chat_id'),
             'text' => $text,
-            'parse_mode' => 'Markdown', // 'HTML',
+            'parse_mode' => 'Markdown',
         ]);
     }
 }
