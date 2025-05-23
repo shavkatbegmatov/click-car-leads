0183f8572e75377097a2d483010d47266d71fa93
SHAVKAT BEGMATOV <62874217+shavkatbegmatov@users.noreply.github.com>
2025-05-23 09:12:54 +0500

    del unsused

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -6,7 +6,6 @@ use App\Events\LeadStatusChanged;
 use App\Events\NewLeadCreated;
 use App\Http\Controllers\Controller;
 use App\Models\Lead;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
